4f2a740915c47c136a418295e28c03cb871c7080
andrewjrangel <andrewjrangel@gmail.com>
2013-06-15 09:42:23 -0500

    Initial Commit

---
+++ OnBlast/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OnBlast
+//
+//  Created by Ventura Rangel on 6/15/13.
+//  Copyright (c) 2013 Antifragile Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OnBlast/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OnBlast
+//
+//  Created by Ventura Rangel on 6/15/13.
+//  Copyright (c) 2013 Antifragile Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
