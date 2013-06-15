05dba51e17e210c90496bda773b6438015dca69d
andrewjrangel <andrewjrangel@gmail.com>
2013-06-15 15:26:03 -0500

    Have the team names, colors, cities working

--- MainViewController.m
+++ MainViewController.m
@@ -37,10 +37,75 @@
 }
 
 - (IBAction)searchButtonPressed:(id)sender{
-    NSString *encodedString = [self.searchTextField.text stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    NSString *jsonEscapeString = @"%2Fjson";
-    self.searchString = [NSString stringWithFormat:@"http://api.espn.com/v1/sports/basketball/nba/teams/2?_accept=application%@&apikey=%@", jsonEscapeString, espnAPIKey];
+    [self.searchTextField resignFirstResponder];
     
+    NSString *encodedString = [self.searchTextField.text stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    if ([encodedString isEqualToString:@"Hawks"]) {
+        self.teamID = @"1";
+    } else if ([encodedString isEqualToString:@"Celtics"]) {
+        self.teamID = @"2";
+    } else if ([encodedString isEqualToString:@"Pelicans"]) {
+       self.teamID = @"3";
+    } else if ([encodedString isEqualToString:@"Bulls"]) {
+        self.teamID = @"4";
+    } else if ([encodedString isEqualToString:@"Cavaliers"]) {
+        self.teamID = @"5";
+    } else if ([encodedString isEqualToString:@"Mavericks"]) {
+        self.teamID = @"6";
+    } else if ([encodedString isEqualToString:@"Nuggets"]) {
+        self.teamID = @"7";
+    } else if ([encodedString isEqualToString:@"Pistons"]) {
+        self.teamID = @"8";
+    } else if ([encodedString isEqualToString:@"Warriors"]) {
+        self.teamID = @"9";
+    } else if ([encodedString isEqualToString:@"Rockets"]) {
+        self.teamID = @"10";
+    } else if ([encodedString isEqualToString:@"Pacers"]) {
+        self.teamID = @"11";
+    } else if ([encodedString isEqualToString:@"Clippers"]) {
+        self.teamID = @"12";
+    } else if ([encodedString isEqualToString:@"Lakers"]) {
+        self.teamID = @"13";
+    } else if ([encodedString isEqualToString:@"Heat"]) {
+        self.teamID = @"14";
+    } else if ([encodedString isEqualToString:@"Bucks"]) {
+        self.teamID = @"15";
+    } else if ([encodedString isEqualToString:@"Timberwolves"]) {
+        self.teamID = @"16";
+    } else if ([encodedString isEqualToString:@"Nets"]) {
+        self.teamID = @"17";
+    } else if ([encodedString isEqualToString:@"Knicks"]) {
+        self.teamID = @"18";
+    } else if ([encodedString isEqualToString:@"Magic"]) {
+        self.teamID = @"19";
+    } else if ([encodedString isEqualToString:@"76ers"]) {
+        self.teamID = @"20";
+    } else if ([encodedString isEqualToString:@"Suns"]) {
+        self.teamID = @"21";
+    } else if ([encodedString isEqualToString:@"Trail Blazers"]) {
+        self.teamID = @"22";
+    } else if ([encodedString isEqualToString:@"Kings"]) {
+        self.teamID = @"23";
+    } else if ([encodedString isEqualToString:@"Spurs"]) {
+        self.teamID = @"24";
+    } else if ([encodedString isEqualToString:@"Thunder"]) {
+        self.teamID = @"25";
+    }else if ([encodedString isEqualToString:@"Jazz"]) {
+        self.teamID = @"26";
+    }else if ([encodedString isEqualToString:@"Wizards"]) {
+        self.teamID = @"27";
+    }else if ([encodedString isEqualToString:@"Raptors"]) {
+        self.teamID = @"28";
+    }else if ([encodedString isEqualToString:@"Grizzlies"]) {
+        self.teamID = @"29";
+    }else if ([encodedString isEqualToString:@"Bobcats"]) {
+        self.teamID = @"30";
+    }
+        
+    
+    
+    NSString *jsonEscapeString = @"%2Fjson";
+    self.searchString = [NSString stringWithFormat:@"http://api.espn.com/v1/sports/basketball/nba/teams/%@?_accept=application%@&apikey=%@",self.teamID, jsonEscapeString, espnAPIKey];
     
     self.url = [NSURL URLWithString:self.searchString];
     self.request = [NSURLRequest requestWithURL:self.url];
@@ -52,7 +117,6 @@
         NSDictionary *stepOne = [self.resultsArray objectAtIndex:0];
         NSArray *stepTwo = [stepOne objectForKey:@"leagues"];
         NSArray *stepThree = [[stepTwo objectAtIndex:0]objectForKey:@"teams"];
-        self.teamID = [[stepThree objectAtIndex:0]objectForKey:@"id"];
         NSArray *stepFour = [[[stepThree objectAtIndex:0] objectForKey:@"links"]objectForKey:@"api"];
         self.espnNewsArray = [[[[[stepThree objectAtIndex:0] objectForKey:@"links"]objectForKey:@"api"] objectForKey:@"news"] objectForKey:@"href"];
         self.espnNotesArray = [[[[[stepThree objectAtIndex:0] objectForKey:@"links"]objectForKey:@"api"] objectForKey:@"notes"] objectForKey:@"href"];
@@ -113,41 +177,89 @@
         } else if ([self.teamName isEqualToString:@"Celtics"]) {
             self.teamNameLabel.textColor = UIColorFromRGB(0x006532);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        } else if ([self.teamName isEqualToString:@"Pelicans"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x0093B1);
+            
+        } else if ([self.teamName isEqualToString:@"Bulls"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x000000);
+            
+        } else if ([self.teamName isEqualToString:@"Cavaliers"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x061642);
+            
+        } else if ([self.teamName isEqualToString:@"Mavericks"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x0C479D);
+            
+        } else if ([self.teamName isEqualToString:@"Nuggets"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x0860A8);
+            
+        } else if ([self.teamName isEqualToString:@"Pistons"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0xFA002C);
+            
+        } else if ([self.teamName isEqualToString:@"Warriors"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x00275D);
+            
+        } else if ([self.teamName isEqualToString:@"Rockets"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0xD40026);
+            
+        } else if ([self.teamName isEqualToString:@"Pacers"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x061642);
+            
+        } else if ([self.teamName isEqualToString:@"Clippers"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0xFA0028);
             
         } else if ([self.teamName isEqualToString:@"Hawks"]) {
             self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        } else if ([self.teamName isEqualToString:@"Lakers"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x542582);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        }else if ([self.teamName isEqualToString:@"Heat"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x000000);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        }else if ([self.teamName isEqualToString:@"Bucks"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x003813);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        }else if ([self.teamName isEqualToString:@"Timberwolves"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x0E3764);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        }else if ([self.teamName isEqualToString:@"Nets"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x06143F);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        }else if ([self.teamName isEqualToString:@"Knicks"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x225EA8);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        }else if ([self.teamName isEqualToString:@"Magic"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x0860A8);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        }else if ([self.teamName isEqualToString:@"76ers"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x000000);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        }else if ([self.teamName isEqualToString:@"Suns"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x23006A);
             
-        } else if ([self.teamName isEqualToString:@"Hawks"]) {
-            self.teamNameLabel.textColor = UIColorFromRGB(0x002B5C);
+        }else if ([self.teamName isEqualToString:@"Kings"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x393996);
+            
+        }else if ([self.teamName isEqualToString:@"Spurs"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x000000);
+            
+        }else if ([self.teamName isEqualToString:@"Thunder"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0xC67C03);
+            
+        }else if ([self.teamName isEqualToString:@"Jazz"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x06143F);
+            
+        }else if ([self.teamName isEqualToString:@"Wizards"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x0E3764);
+            
+        }else if ([self.teamName isEqualToString:@"Raptors"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0xCE0F41);
+            
+        }else if ([self.teamName isEqualToString:@"Grizzlies"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0x5D76A8);
+            
+        }else if ([self.teamName isEqualToString:@"Bobcats"]) {
+            self.teamNameLabel.textColor = UIColorFromRGB(0xFE3310);
             
         }
      
